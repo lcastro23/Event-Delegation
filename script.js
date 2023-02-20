002267b926b0be1b0bf34b0a31d2787020b83e4d
Laur <laurcastro.l.c@gmail.com>
2023-02-20 14:52:47 -0500

    First Commit

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function show_alert() {
-    alert("You clicked the submit button! Sorry, your submission doesn't actually go anywhere :)");
-  }
+
+		let nameForm = document.getElementById("nameForm");
+		nameForm.addEventListener("submit", onSubmit);
+
+		let buttonsDiv = document.createElement("div");
+		buttonsDiv.id = "buttons";
+		buttonsDiv.addEventListener("click", onClick);
+		document.body.appendChild(buttonsDiv);
+
+		function onSubmit(event)
+		{
+			let newButton = document.createElement("button");
+			newButton.textContent = document.getElementById("name").value;
+			buttonsDiv.appendChild(newButton);
+
+			event.preventDefault();
+		}
+
+		function onClick(event)
+		{
+			if(event.target.matches("button") == true)
+			{
+				console.log(event.target.textContent);
+			}
+		}
